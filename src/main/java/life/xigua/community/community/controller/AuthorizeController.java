e590a864ad4dd736a316e1b393dfb91a17048ecc
wzf <aa136142877@qq.com>
2019-07-24 17:49:49 +0800

    add check cookies

--- src/main/java/life/xigua/community/community/controller/AuthorizeController.java
+++ src/main/java/life/xigua/community/community/controller/AuthorizeController.java
@@ -3,12 +3,18 @@ package life.xigua.community.community.controller;
 
 import life.xigua.community.community.dto.AccessTokenDTO;
 import life.xigua.community.community.dto.GithubUser;
+import life.xigua.community.community.mapper.UserMapper;
+import life.xigua.community.community.model.User;
 import life.xigua.community.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -25,11 +31,14 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
+    @Autowired
+    private UserMapper userMapper;
 
 
         @GetMapping("/callback")
     public String callback(@RequestParam( name = "code") String code,
-                           @RequestParam(name = "state") String state){
+                           @RequestParam(name = "state") String state,
+                           HttpServletResponse response){
             AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
             accessTokenDTO.setClient_id(cliendId);
             accessTokenDTO.setClient_secret(cliendSecret);
@@ -37,8 +46,24 @@ public class AuthorizeController {
             accessTokenDTO.setRedirect_uri(redirectUri);
             accessTokenDTO.setState(state);
             String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-            GithubUser user = githubProvider.getUser(accessToken);
-            System.out.println(user.getName());
-            return "index";
+            GithubUser githubUser = githubProvider.getUser(accessToken);
+
+            if (githubUser != null){
+                User user = new User();
+                String token = UUID.randomUUID().toString();
+                user.setToken(token);
+                user.setName(githubUser.getName());
+                user.setAccountId(String.valueOf(githubUser.getId()));
+                user.setGmtCreate(System.currentTimeMillis());
+                user.setGmtModified(user.getGmtCreate());
+                userMapper.insert(user);
+                response.addCookie(new Cookie("token",token));
+                return "redirect:/";
+                //登录成功，写cookie和session
+            } else {
+                //登录失败，重新登录
+                return "redirect:/";
+            }
+
         }
 }
