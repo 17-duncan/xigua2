be6f83a0b4cc8029b3ced9d9944d4c506f7accc8
wzf <aa136142877@qq.com>
2019-07-24 15:29:41 +0800

    extract github config from code to properties

--- src/main/java/life/xigua/community/community/controller/AuthorizeController.java
+++ src/main/java/life/xigua/community/community/controller/AuthorizeController.java
@@ -5,6 +5,7 @@ import life.xigua.community.community.dto.AccessTokenDTO;
 import life.xigua.community.community.dto.GithubUser;
 import life.xigua.community.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,14 +16,25 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String cliendId;
+
+    @Value("${github.cliend.secret}")
+    private String cliendSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+
+
         @GetMapping("/callback")
     public String callback(@RequestParam( name = "code") String code,
                            @RequestParam(name = "state") String state){
             AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-            accessTokenDTO.setClient_id("c262f46212b251232578");
-            accessTokenDTO.setClient_secret("f0bd0ffe17d639e80d7003baac8bf573e57902b3");
+            accessTokenDTO.setClient_id(cliendId);
+            accessTokenDTO.setClient_secret(cliendSecret);
             accessTokenDTO.setCode(code);
-            accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+            accessTokenDTO.setRedirect_uri(redirectUri);
             accessTokenDTO.setState(state);
             String accessToken = githubProvider.getAccessToken(accessTokenDTO);
             GithubUser user = githubProvider.getUser(accessToken);
